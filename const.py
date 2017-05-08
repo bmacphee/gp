6ee516313fa1070b5250c168b5dbcf7be1556ef8
Selene <sjbance@gmail.com>
2017-05-08 15:18:42 -0300

    Additions to prog, edit vm

--- const.py
+++ const.py
@@ -1,5 +1,17 @@
+from enum import Enum
+
 TARGET = 0
 SOURCE = 1
 OP = 2
 MODE = 3
 PROG_LENGTH = 48
+TOURNAMENT_SIZE = 4
+GEN_REGS = 8
+
+class StandardizeMethod(Enum):
+    MEAN_VARIANCE = 0
+    LINEAR_TRANSFORM = 1
+
+class Selection(Enum):
+    STEADY_STATE_TOURN = 0
+    BREEDER_MODEL = 1
--- vm.py
+++ vm.py
@@ -1,7 +1,5 @@
-import random
-import pdb
-import const
-
+import random, pdb, const
+from numpy import sqrt
 
 class Vm:
     def __init__(self, num_genregs, num_ipregs, num_ops, output_dims):
@@ -10,19 +8,26 @@ class Vm:
         self.num_ops = num_ops
         self.output_dims = output_dims
         self.gen_regs = [0]*num_genregs
+        # mode=0: general regs, mode=1: ip regs
+        self.mode = {
+            0: (lambda x:self.gen_regs[x % self.num_genregs]),
+            1: (lambda x:self.ip_regs[x % self.num_ipregs])
+        }
+    @profile
+    def run_prog(self, prog, ip, remove_introns=True):
+        self.prog_len = len(prog[0])
+        self.ip_regs = [float(x) for x in ip]
+        target_col = prog[const.TARGET]
+        source_col = prog[const.SOURCE]
+        mode_col = prog[const.MODE]
+        op_col = prog[const.OP]
+        effective_instrs = self.find_introns(target_col, source_col, mode_col) if remove_introns else range(self.prog_len)
 
-    def run_prog(self, prog, ip):
-        for i in range(0, len(prog[0])):
-            target_ind = prog[const.TARGET][i]
-            ip_regs = [float(x) for x in ip]
-            # mode=0 --> general regs, mode=1 --> ip regs
-            if prog[const.MODE][i] == 0:
-                source = self.gen_regs[prog[const.SOURCE][i] % self.num_genregs]
-            elif prog[const.MODE][i] == 1:
-                source = ip_regs[prog[const.SOURCE][i] % self.num_ipregs]
-            else:
-                raise ValueError('Invalid mode')
-            self.gen_regs[target_ind] = do_op(prog[const.OP][i], self.gen_regs[target_ind], source)
+        for i in sorted(effective_instrs):
+            target_ind = target_col[i]
+            source = self.mode[mode_col[i]](source_col[i])
+            self.gen_regs[target_ind] = do_op(op_col[i], self.gen_regs[target_ind], source)
+
         output = self.gen_regs[:self.output_dims]
         self.clear()
         return output
@@ -30,19 +35,37 @@ class Vm:
     def clear(self):
         self.gen_regs = [0]*self.num_genregs
 
+    # Return set w/ indices of effective instructions
+    @profile
+    def find_introns(self, target, source, mode):
+        marked_instrs = set()
+        eff_regs = set(range(self.output_dims))
+        instrs = reversed(range(self.prog_len-1))
+        for i in instrs:
+            if target[i] in eff_regs and mode[i] == 0:
+                marked_instrs.add(i)
+                eff_regs.add(source[i] % self.num_genregs)
+        return marked_instrs
+
+
+def add(target, source):
+    return target + source
+
+def sub(target, source):
+    return target - source
+
+def mult(target, source):
+    return target * source
+
+def div(target, source):
+    return target / (sqrt(1+source**2))
+
+ops = { 0: add, 1: sub, 2: mult, 3: div }
 
 def do_op(op_code, target, source):
-    if op_code == 0:
-        val = (target + source)
-    elif op_code == 1:
-        val = (target - source)
-    elif op_code == 2:
-        val = (target * source)
-    elif op_code == 3:
-        if source != 0:
-            val = (target / source)
-        else:
-            val = target
-    else:
-        raise ValueError('Invalid op')
+    try:
+        val = ops[op_code](target, source)
+    except Exception as e:
+        print(e)
+        val = target
     return val
